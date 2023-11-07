5a90dbb5d747395fbfa71f2cc6ff3124a77ec329
David <mcreeps464@gmail.com>
2023-11-07 10:08:05 +0400

    Commit

--- source.py
+++ source.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import solve_ivp
 
-def func(t, r, W, W0, sigma, d, G, g):
-    W[0] = W0[0]* np.e ** -((t - t0[0]) /2*sigma[0]) ** 2
-    W[1] = W0[1]* np.e ** -((t - t0[1]) /2*sigma[1]) ** 2
+def func(t, r, W, W0, sigma, d, G, g, t0):
     r = np.asarray(r, dtype=np.complex128)
     drdt = np.zeros_like(r, dtype=np.complex128)
+    W[0] = W0[0]* np.e ** -(((t - t0[0]) /2*sigma[0]) ** 2)
+    W[1] = W0[1]* np.e ** -(((t - t0[1]) /2*sigma[1]) ** 2)
     drdt[0] = G[3]*r[4] - 1j*W[0]*(r[3] - r[1])
     drdt[4] = -(G[3] + G[5])*r[4] - 1j*(W[0]*(r[1] - r[3]) + W[1]*(r[7] - r[5]))
     drdt[8] = G[5]*r[4] - 1j*W[1]*(r[5] - r[7])
@@ -47,7 +47,7 @@ d = np.zeros(2)
 r0 = [1.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j, 0.0 + 0.0j]
 
 t_span = [0.0, 50.0]
-args = (W, W0, sigma, d, G, g)
+args = (W, W0, sigma, t0, d, G, g)
 
 sol = solve_ivp(func, t_span, r0, method="RK45", args=args)
 
